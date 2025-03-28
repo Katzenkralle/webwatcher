d45feb4630fa282672222057d6c1d81611e634d0
DerVogel101 <jan.irmer@outlook.de>
2025-03-28 07:52:24 +0000

    refactor: update __main__.py to call main() and comment out previous function calls

--- webw_serv/__main__.py
+++ webw_serv/__main__.py
@@ -3,5 +3,6 @@ from webw_serv.watcher.manager import manager_main
 from webw_serv.watcher.script_checker import script_checker
 
 if __name__ == "__main__":
-    print(script_checker("http_return"))
+    # print(script_checker("http_return"))
     # manager_main()
+    main()
