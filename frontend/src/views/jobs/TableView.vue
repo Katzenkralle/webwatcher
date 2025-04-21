d8afcb6f8c8294caa0f8d5cb32a5be40282da7ff
katzenkralle <some@one>
2025-04-21 20:22:59 +0000

    improve layout and styling in MetaData and TableView components

--- frontend/src/views/jobs/TableView.vue
+++ frontend/src/views/jobs/TableView.vue
@@ -65,94 +65,95 @@ const graphCoardinator = ref();
 
 <template>
   <main>
-    <div :key="currentJobId">
-      <div class="flex flex-wrap justify-between mb-2 mt-5">
-        <NavButtons />
-        <h1 class="self-center">Job Data & Managment</h1>
-        <p></p>
-      </div>
-      <Accordion 
-        class="w-full max-w-256 mb-2"
-        :value="['0']" 
-        multiple
-        unstyled>
+    <div :key="currentJobId" class="!max-w-full flex flex-col justift-center items-center">
+      <div class="small-box">
+        <div class="flex flex-wrap justify-between mb-2 mt-5">
+          <NavButtons />
+          <h1 class="self-center">Job Data & Managment</h1>
+          <p></p>
+        </div>
+        <Accordion 
+          class="w-full mb-2"
+          :value="['0']" 
+          multiple
+          unstyled>
 
-        <AccordionPanel value="0">
-            <AccordionHeader>Overview</AccordionHeader>
-              <AccordionContent>
-                <JobMetaDisplay 
-                  v-if="jobHandler?.metaData.value"
-                  :metaData="jobHandler.metaData.value"
-                  />
-              </AccordionContent>
-        </AccordionPanel>
-
-        <AccordionPanel value="1">
-            <AccordionHeader>Graph Creator</AccordionHeader>
-              <AccordionContent>
-                <div class="content-box flex flex-col">
-                    <GraphInput
-                      ref="graphCoardinator"
-                      :jobData="jobHandler.jobDataHandler"
-                      :user-config="userConfig"
+          <AccordionPanel value="0">
+              <AccordionHeader>Overview</AccordionHeader>
+                <AccordionContent>
+                  <JobMetaDisplay 
+                    v-if="jobHandler?.metaData.value"
+                    :metaData="jobHandler.metaData.value"
                     />
-              </div>
-              </AccordionContent>
-        </AccordionPanel>
+                </AccordionContent>
+          </AccordionPanel>
 
-        <AccordionPanel value="2">
-            <AccordionHeader>Regression</AccordionHeader>
-              <AccordionContent>
-                <div class="content-box flex flex-col">
-                  <Regression
-                    :data-handler="jobHandler.jobDataHandler"
-                    />
-              </div>
-              </AccordionContent>
-        </AccordionPanel>
-
-        <AccordionPanel value="3">
-            <AccordionHeader>Logical Filters</AccordionHeader>
-              <AccordionContent>
-                <div class="content-box flex flex-col">
-                    <FilterSelector
-                      :filterContext="jobHandler.filterContext"
-                      :userConfig="userConfig"
+          <AccordionPanel value="1">
+              <AccordionHeader>Graph Creator</AccordionHeader>
+                <AccordionContent>
+                  <div class="content-box flex flex-col">
+                      <GraphInput
+                        ref="graphCoardinator"
+                        :jobData="jobHandler.jobDataHandler"
+                        :user-config="userConfig"
                       />
-                    <SmallSeperator 
-                      class="my-2 mx-auto" 
-                      :is-dashed="true"/>
-                    <FilterGroupRenderer 
-                      :jobHandler="jobHandler.jobDataHandler"/>
-              </div>
-              </AccordionContent>
-        </AccordionPanel>
-
-        <AccordionPanel value="4">
-            <AccordionHeader>View Options</AccordionHeader>
-              <AccordionContent>
-                <div class="content-box shrinkable">
-                  <ColumnSelection
-                    :hiddenColumns="jobHandler?.hiddenColumns.value"
-                    :allColumns="jobHandler.jobDataHandler.computeLayoutUnfiltered.value"
-                    :visableColumns="jobHandler.jobDataHandler.computeLayout.value"
-                    :internalColumns="jobHandler.intenalColums"
-                    @update:hiddenColumns="(e) => { if (jobHandler) 
-                        jobHandler.hiddenColumns.value = e }"
-                    />
-
-                  <StringSearch
-                    v-if="jobHandler.sortByString.value"
-                    :mutSortByString="jobHandler?.sortByString.value"
-                    :allColumns="jobHandler.jobDataHandler.computeLayoutUnfiltered.value"
-                    @update:key="() => jobHandler?.unsetPrimevueSort() "
-                    class="shrink-0"
-                    />
                 </div>
-              </AccordionContent>
-        </AccordionPanel>
-      </Accordion>
+                </AccordionContent>
+          </AccordionPanel>
 
+          <AccordionPanel value="2">
+              <AccordionHeader>Regression</AccordionHeader>
+                <AccordionContent>
+                  <div class="content-box flex flex-col">
+                    <Regression
+                      :data-handler="jobHandler.jobDataHandler"
+                      />
+                </div>
+                </AccordionContent>
+          </AccordionPanel>
+
+          <AccordionPanel value="3">
+              <AccordionHeader>Logical Filters</AccordionHeader>
+                <AccordionContent>
+                  <div class="content-box flex flex-col">
+                      <FilterSelector
+                        :filterContext="jobHandler.filterContext"
+                        :userConfig="userConfig"
+                        />
+                      <SmallSeperator 
+                        class="my-2 mx-auto" 
+                        :is-dashed="true"/>
+                      <FilterGroupRenderer 
+                        :jobHandler="jobHandler.jobDataHandler"/>
+                </div>
+                </AccordionContent>
+          </AccordionPanel>
+
+          <AccordionPanel value="4">
+              <AccordionHeader>View Options</AccordionHeader>
+                <AccordionContent>
+                  <div class="content-box shrinkable">
+                    <ColumnSelection
+                      :hiddenColumns="jobHandler?.hiddenColumns.value"
+                      :allColumns="jobHandler.jobDataHandler.computeLayoutUnfiltered.value"
+                      :visableColumns="jobHandler.jobDataHandler.computeLayout.value"
+                      :internalColumns="jobHandler.intenalColums"
+                      @update:hiddenColumns="(e) => { if (jobHandler) 
+                          jobHandler.hiddenColumns.value = e }"
+                      />
+
+                    <StringSearch
+                      v-if="jobHandler.sortByString.value"
+                      :mutSortByString="jobHandler?.sortByString.value"
+                      :allColumns="jobHandler.jobDataHandler.computeLayoutUnfiltered.value"
+                      @update:key="() => jobHandler?.unsetPrimevueSort() "
+                      class="shrink-0"
+                      />
+                  </div>
+                </AccordionContent>
+          </AccordionPanel>
+        </Accordion>
+      </div>
         
       <div class="flex flex-col w-full h-full items-center">
         <SmallSeperator class="my-4" :is-dashed="true"/>
@@ -169,51 +170,55 @@ const graphCoardinator = ref();
         />
       </div>
 
-      <PopupDialog ref="downloadSuccessPopup"
-        title="Success"
-        message="Data was saved to file."
-        >
-        <template #footer>
-          <Button
-            label="Close"
-            size="small"
-            @click="() => downloadSuccessPopup?.closeDialog()"
-            />
-        </template>
-      </PopupDialog>
-      <div class="flex flex-row space-x-2 ml-auto mr-4 my-2">
-        <ConfirmableButton
-            button-label="Fetch all Data"
-            button-icon="pi pi-refresh"
-            confirm-message="Are you sure you want to fetch all data? This may take a while."
-            confirm-icon="pi pi-exclamation-triangle text-warning"
-            @confirm="() => jobHandler?.jobDataHandler.lazyFetch(0, true)"
-            />
-        
-            <SplitButton
-            label="Export visible"
-            icon="pi pi-download"
-            @click="() => { jobHandler?.jobDataHandler.saveToFile('visable', jobHandler?.hiddenColumns.value)
-                  .then(() => downloadSuccessPopup?.openDialog()) }"
-            :model="[
-              {
-                label: 'Export fetched',
-                icon: 'pi pi-download',
-                command: () => { jobHandler?.jobDataHandler.saveToFile('all')
-                  .then(() => downloadSuccessPopup?.openDialog()) }
-              }
-            ]"
-            />
-
+      <div class="small-box">
+        <PopupDialog ref="downloadSuccessPopup"
+          title="Success"
+          message="Data was saved to file."
+          >
+          <template #footer>
+            <Button
+              label="Close"
+              size="small"
+              @click="() => downloadSuccessPopup?.closeDialog()"
+              />
+          </template>
+        </PopupDialog>
+        <div class="flex flex-row justify-center space-x-2 ml-auto mr-4 my-2">
+          <ConfirmableButton
+              button-label="Fetch all Data"
+              button-icon="pi pi-refresh"
+              confirm-message="Are you sure you want to fetch all data? This may take a while."
+              confirm-icon="pi pi-exclamation-triangle text-warning"
+              @confirm="() => jobHandler?.jobDataHandler.lazyFetch(0, true)"
+              />
+          
+              <SplitButton
+              label="Export visible"
+              icon="pi pi-download"
+              @click="() => { jobHandler?.jobDataHandler.saveToFile('visable', jobHandler?.hiddenColumns.value)
+                    .then(() => downloadSuccessPopup?.openDialog()) }"
+              :model="[
+                {
+                  label: 'Export fetched',
+                  icon: 'pi pi-download',
+                  command: () => { jobHandler?.jobDataHandler.saveToFile('all')
+                    .then(() => downloadSuccessPopup?.openDialog()) }
+                }
+              ]"
+              />
+        </div>
       </div>
 
     </div>
   </main>
 </template>
 
-<style lang="css">
+<style scoped>
 @reference "@/assets/global.css";
 
+.small-box {
+  @apply max-w-256 w-full
+}
 
 .shrinkable {
   @apply flex 
