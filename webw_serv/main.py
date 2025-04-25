688e8a1c6decc615757428aa1d8a1b8db29b062c
Adminosaurus <adminosaurus@dervogel101.de>
2025-04-25 13:39:38 +0000

    Refactor DB connection logic into a separate module
    Extracted the `establish_db_connections` function from `main.py` into a new module, `main_establish_dbs.py`, to improve modularity and maintainability. Removed the redundant definition in `main.py` and updated imports accordingly. This change simplifies the `main.py` file, making it more focused on its core responsibilities.

--- webw_serv/main.py
+++ webw_serv/main.py
@@ -5,6 +5,7 @@ from apscheduler.triggers.cron import CronTrigger
 from contextlib import asynccontextmanager
 
 from webw_serv import CONFIG
+from webw_serv.main_establish_dbs import establish_db_connections
 from webw_serv.utility.oven_cleaner import cleanup_folder
 from webw_serv.API.core import get_routes
 from webw_serv.configurator import Config
@@ -17,22 +18,6 @@ from webw_serv.db_handler import MongoDbHandler, MariaDbHandler
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
-
-
-def establish_db_connections():
-    # Setup everything
-    mongo = MongoDbHandler(Config().mongo)
-    maria = MariaDbHandler(Config().maria, Config().app)
-    
-    async def registerSession():
-        jobs = await maria.get_job_metadata()
-        await mongo.register_all_sql_jobs([job.id for job in jobs])
-    try:
-        asyncio.run(registerSession())
-    except Exception as e:
-        DEFAULT_LOGGER.error(f"Failed to register SQL jobs: {e}")
-        
-    return [mongo, maria]
 
 def generate_scheduler():
     scheduler = BackgroundScheduler(
